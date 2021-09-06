ddd71e8fbc5768ffcab55d8972823f70f59dd9a9
Madhav Varshney <madhavarshney@gmail.com>
2021-09-05 17:24:56 -0700

    First version of extension - workaround SAML issues

---
+++ README.md
@@ -0,0 +1,21 @@
+# FHDA MyPortal Patcher
+
+Patches the Foothill-De Anza MyPortal & SSB to fix common issues.
+
+Currently fixes SAML validation errors, such as "Error validating SAML message".
+
+## Installation
+
+For now, the extension needs to be manually installed as it is not published in the webstore.
+
+Instructions for Chrome:
+
+1. [Download the .zip](zipball/main/) and unpack it
+2. Go to [chrome://extensions](chrome://extensions)
+3. Turn on developer mode (toggle the switch in the top right corner)
+4. Click "Load Unpacked" and choosed the unpacked folder to add the extension
+5. Enjoy (and please report bugs!)
+
+## License
+
+[MIT License](LICENSE.md), built by Madhav Varshney.
---
+++ background.js
@@ -0,0 +1,97 @@
+const DEBUG_MODE = false
+
+const cookiesToDelete = [
+  { name: 'commonAuthId', url: 'https://eis-prod.ec.fhda.edu/', domain: 'eis-prod.ec.fhda.edu' },
+  { name: 'SESSID', url: 'https://ssb-prod.ec.fhda.edu/PROD', domain: 'ssb-prod.ec.fhda.edu' }
+]
+const eisCookie = cookiesToDelete[0]
+const deleteCookiesCmd = 'DELETE_COOKIES'
+
+/**
+ * Simple log function that prepends a timestamp
+ */
+function log(...params) {
+  console.log(new Date(Date.now()).toISOString(), ...params)
+}
+
+/**
+ * Delete cookies that cause SAML validation errors when stale
+ */
+async function deleteBadCookies() {
+  const cookiesDeleted = await Promise.all(
+    cookiesToDelete.map(({ name, url }) => chrome.cookies.remove({ name, url }))
+  )
+
+  log('Deleted cookies', cookiesDeleted)
+}
+
+/**
+ * Schedule an alarm to delete cookies that should expire
+ */
+async function scheduleAlarm() {
+  const delayInMinutes = DEBUG_MODE ? 1 : 120 - 5
+
+  await clearAlarms()
+  await chrome.alarms.create(deleteCookiesCmd, { delayInMinutes })
+
+  log(`Scheduled alarm to clear cookies in ${delayInMinutes} minute(s).`)
+}
+
+/**
+ * Clear alarms to delete cookies
+ */
+async function clearAlarms() {
+  const wasCleared = await chrome.alarms.clear(deleteCookiesCmd)
+
+  if (wasCleared) {
+    log(`Cleared alarm to delete cookies.`)
+  }
+}
+
+/**
+ * Handle alarm
+ */
+chrome.alarms.onAlarm.addListener(({ name }) => {
+  if (name === deleteCookiesCmd) {
+    clearAlarms()
+    deleteBadCookies()
+  }
+})
+
+/**
+ * When the cookie `commonAuthId` on eis-prod.ec.fhda.edu is added or removed,
+ * schedule or clear the delete cookies alarm (which deletes the cookies after a timeout)
+ */
+chrome.cookies.onChanged.addListener(({ cause, cookie, removed }) => {
+  if (eisCookie.domain !== cookie.domain || eisCookie.name !== cookie.name) {
+    return
+  }
+
+  if (cause === 'explicit' && !removed) {
+    log(`Cookie ${cookie.name} on ${cookie.domain} was added.`, cookie)
+    scheduleAlarm()
+  }
+
+  if (removed) {
+    log(`Cookie ${cookie.name} on ${cookie.domain} was removed.`, cookie)
+    clearAlarms()
+  }
+})
+
+/**
+ * In debug mode, delete cookies when the extension icon is clicked
+ *
+ * TODO: remove to so that action: {} can be removed in manifest.json
+ */
+if (DEBUG_MODE) {
+  chrome.action.onClicked.addListener(() => deleteBadCookies())
+}
+
+/**
+ * Delete bad cookies on load when not debugging
+ */
+if (!DEBUG_MODE) {
+  deleteBadCookies()
+}
+
+log(`FHDA MyPortal Patcher booted up, debug mode is ${DEBUG_MODE ? 'on' : 'off'}.`)
